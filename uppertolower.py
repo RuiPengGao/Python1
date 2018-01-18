e606dc5d4c1d3cf61859ecf2f911e1e8387640f4
RuiPengGao <35279563+RuiPengGao@users.noreply.github.com>
2018-01-18 21:13:50 +0800

    Add files via upload

---
+++ uppertolower.py
@@ -0,0 +1,10 @@
+# Python Program - Convert Uppercase to Lowercase
+		
+while True:
+	print("Enter 'stop' for exit.")
+	string = input("Enter any string: ")
+	if string == 'stop':
+		break
+	else:
+		string_in_lowercase = string.lower()
+		print("String in Lowercase =",string_in_lowercase,"\n")
