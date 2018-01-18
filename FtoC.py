bafbd92f1d84e5612f294eb1952312bc2fd29352
RuiPengGao <35279563+RuiPengGao@users.noreply.github.com>
2018-01-18 21:02:01 +0800

    Delete FtoC.py

--- FtoC.py
+++
@@ -1,7 +0,0 @@
-#converting fahrenheit to celsius
-
-Fahrenheit = float(input("Enter Fahrenheit: "))
-
-celsius = (5/9) * (fahrenheit - 32)
-
-print("{0}°F is converted to {1:1.1f}°C.".format(fahrenheit,celsius))
